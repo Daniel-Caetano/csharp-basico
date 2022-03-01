9ec7335ac637992d1c382e61ef7cc2cb590d22bc
Daniel Caetano de Souza Ferreira <91154445+Daniel-Caetano@users.noreply.github.com>
2022-03-01 10:09:34 -0300

    Iniciando projeto da parte básica do C#

---
+++ Program/Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando projeto da parte básica do C#");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+Autor: Daniel Caetano de Souza Ferreira
+Data: 01/03/2022
+
 # csharp-basico
 Projeto para fixação do conteúdo do curso de c#-completo na Udemy
